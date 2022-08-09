55479fb216b6fd5e4aaba9395ded9dbc65b4d60b
Rafael <rldcarvalhoo@gmail.com>
2022-08-09 19:25:49 -0300

    Database configuration

---
+++ src/main/java/br/com/rldcarvalho/controlefinanceiroapi/ControleFinanceiroApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.rldcarvalho.controlefinanceiroapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleFinanceiroApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleFinanceiroApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/rldcarvalho/controlefinanceiroapi/modelo/entity/Receitas.java
@@ -0,0 +1,18 @@
+package br.com.rldcarvalho.controlefinanceiroapi.modelo.entity;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+public class Receitas {
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String descricao;
+    private BigDecimal valor;
+    private LocalDate data;
+
+
+}
