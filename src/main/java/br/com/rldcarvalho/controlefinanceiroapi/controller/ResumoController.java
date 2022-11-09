fcfafee20c3d702b8cd477869037a00e2fc18734
Rafael <rldcarvalhoo@gmail.com>
2022-11-08 22:46:37 -0300

    Fix CategoriaDto JsonOrder

--- src/main/java/br/com/rldcarvalho/controlefinanceiroapi/controller/ResumoController.java
+++ src/main/java/br/com/rldcarvalho/controlefinanceiroapi/controller/ResumoController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
 import java.time.DateTimeException;
 import java.time.LocalDate;
 
--- src/main/java/br/com/rldcarvalho/controlefinanceiroapi/controller/dto/CategoriaDto.java
+++ src/main/java/br/com/rldcarvalho/controlefinanceiroapi/controller/dto/CategoriaDto.java
@@ -1,9 +1,11 @@
 package br.com.rldcarvalho.controlefinanceiroapi.controller.dto;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import org.springframework.beans.factory.annotation.Value;
 
 import java.math.BigDecimal;
 
+@JsonPropertyOrder(alphabetic = true)
 public interface CategoriaDto {
 
     @Value("#{target.categoria.descricao}")
