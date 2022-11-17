272869a336404f92d158811f0125a1dd2302a3eb
Rafael <rldcarvalhoo@gmail.com>
2022-11-16 22:26:36 -0300

    Added prod database config

---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO usuarios(usuario, senha) VALUES ('user', '$2a$10$qbPSQ0szBYoEHadFE3s.r.DKl6zK8mvo.vEW2..wKigcKcTwS9YQy')
+
+INSERT INTO receita(descricao, valor, data) VALUES('Salário', '4000.00', '2022-08-09');
+INSERT INTO receita(descricao, valor, data) VALUES('Renda Extra', '400.00', '2022-08-09');
+
+INSERT INTO despesa(descricao, valor, data, categoria) VALUES('Aluguel', '1400.00', '2022-08-05', 'MORADIA');
+INSERT INTO despesa(descricao, valor, data, categoria) VALUES('Prestação Carro', '700.00', '2022-08-05', 'TRANSPORTE');
+INSERT INTO despesa(descricao, valor, data, categoria) VALUES('Água', '90.00', '2022-08-10', 'MORADIA');
+INSERT INTO despesa(descricao, valor, data, categoria) VALUES('Luz', '140.00', '2022-08-10', 'MORADIA');
+INSERT INTO despesa(descricao, valor, data, categoria) VALUES('Internet', '99.00', '2022-08-15', 'MORADIA');
+INSERT INTO despesa(descricao, valor, data, categoria) VALUES('Celular', '49.00', '2022-08-15', 'OUTRAS');
+INSERT INTO despesa(descricao, valor, data, categoria) VALUES('Academia', '140.00', '2022-08-20', 'LAZER');
+INSERT INTO despesa(descricao, valor, data, categoria) VALUES ('Mercado', '700.00', '2022-08-05', 'ALIMENTAÇÃO');
